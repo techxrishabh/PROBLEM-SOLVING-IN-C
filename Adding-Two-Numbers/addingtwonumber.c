b9976e63bd8beb0114320c2d6ff6a010131d605b
rishabh22204901 <rishabh22204901@gmail.com>
2023-10-11 10:55:06 +0530

    update adding-two-number

--- Adding-Two-Numbers/addingtwonumber.c
+++ Adding-Two-Numbers/addingtwonumber.c
@@ -2,6 +2,11 @@
 int main(){
     int a = 10;
     int b = 20;
-    printf("%d", a + b); //30
+
+    float amount = 30.79 + 60.33;
+
+    int number = a + b; 
+    printf("%d\n", number); //30
+    printf("%f", amount); //91.120003
     return 0;
 }
