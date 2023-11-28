f566f3015d55a4536fc7bbb9413b8218c864dd97
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-28 10:00:59 +0530

    added find the remainder

---
+++ Find-the-Remainder/remainder.c
@@ -0,0 +1,18 @@
+// Take two integers input, a and b : a>b, and find the
+// remainder when a is divided by b.
+
+
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Print a Number a:\n");
+    scanf("%d", &a);
+    printf("Print a Number b:\n");
+    scanf("%d", &b);
+    printf("The remainder is: %d", a/b);
+
+    return 0;
+
+
+
+}
