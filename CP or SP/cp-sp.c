cd73a000ce9f37dad5db845d05bd0db43a1d2278
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-29 09:53:56 +0530

    updated cp or loss problem

--- CP or SP/cp-sp.c
+++ CP or SP/cp-sp.c
@@ -1,3 +1,9 @@
+// If cost price and selling price of an item is
+// input through the keyboard, write a program to
+// determine whether the seller has made profit or
+// incurred loss. Also determine how much profit he
+// made or loss he incurred.
+
 #include <stdio.h>
 int main(){
     int cp, sp;
