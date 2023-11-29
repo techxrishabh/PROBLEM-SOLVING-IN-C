143f786c9db3e8b0d258154584db7a95023fa6d8
rishabh22204901 <rishabh22204901@gmail.com>
2023-11-29 09:52:52 +0530

    updated cp or loss problem

--- CP or SP/cp-sp.c
+++ CP or SP/cp-sp.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
     int cp, sp;
-    printf("What is the cost price\n");
+    printf("What is the cost price?\n");
     scanf("%d", &cp);
-    printf("what is the selling price\n");
+    printf("what is the selling price?\n");
     scanf("%d", &sp);
     if(sp>cp){
         printf("congratulation! you has made a profit");
