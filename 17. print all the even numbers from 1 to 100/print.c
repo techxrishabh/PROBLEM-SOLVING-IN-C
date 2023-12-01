27b6a3c92eadbe2c3179efa86c1cd9aa7364f0cd
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-01 13:21:28 +0530

    added Print all the even numbers from 1 to 100

---
+++ 17. print all the even numbers from 1 to 100/print.c
@@ -0,0 +1,11 @@
+// Print all the even numbers from 1 to 100
+
+#include <stdio.h>
+int main(){
+    int i;
+    for (i=1;i<=100;i++){
+        if(i%2==0){
+            printf("%d\n", i);
+        }
+    }
+}
