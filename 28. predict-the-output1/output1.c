6299a793d7cc151a00d09639b636cd17b761cc8c
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-04 12:12:36 +0530

    added predict the output 1

---
+++ 28. predict-the-output1/output1.c
@@ -0,0 +1,32 @@
+// Predict the output
+
+// main( ) {
+
+// int x = 4, y, z ;
+// y = --x ;
+// z = x-- ;
+// printf ( "\n%d %d %d", x, y, z ) ;
+
+// }
+
+//=======================================================================
+//                              OUTPUT                                 //
+//=======================================================================
+
+// *
+// * 2 3 3
+
+
+// ======================= EXPLAINATION ===============================
+
+// This code is written in C language, which is a low-level programming language that allows direct manipulation of memory and hardware. The code uses some operators and variables to perform some calculations and print the results. Let me explain what each line of the code does:
+
+// main( ) { : This is the start of the main function, which is where the program execution begins.
+// int x = 4, y, z ; : This declares three variables of type int (integer), which can store whole numbers. The variable x is initialized with the value 4, while y and z are not assigned any value yet.
+// y = --x ; : This assigns the value of x minus one to y, and also updates the value of x to be one less. This is called a pre-decrement operator, which means it decrements the value before using it. So, after this line, x becomes 3 and y becomes 3 as well.
+// z = x-- ; : This assigns the current value of x to z, and then decrements the value of x by one. This is called a post-decrement operator, which means it decrements the value after using it. So, after this line, z becomes 3 and x becomes 2.
+// printf ( "\n%d %d %d", x, y, z ) ; : This prints a new line (\n) followed by the values of x, y, and z, separated by spaces. The %d is a format specifier that indicates that the values are integers. So, after this line, the output will be:
+
+// 2 3 3
+
+// } : This is the end of the main function, and the program terminates.
