9529df771051b5f79d04f3a3bc656a06e3fda6e8
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-08 21:33:17 +0530

    added print 1 - 100 by using while loops

---
+++ 29. while-loop/while-loops.c
@@ -0,0 +1,13 @@
+// WAP to print 1 - 100 by using while loops ==================
+
+// =============================================================================
+
+// koi bhi condition ban rhi hhai aur wo toot ti jaa rhi hai
+#include <stdio.h>
+int main(){
+    int i = 1;
+while (i <= 100){  // andar condition likhi hoti hai hai aur jab hame jada chize pata nhi hoti hai tab ham while loops ka use karte hai 
+    printf("%d\n", i);
+    i++;
+}
+}
