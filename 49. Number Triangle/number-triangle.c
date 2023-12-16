d5ba126ddcc67d5d416bfb18e94a3d03f5336c3c
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-16 15:02:08 +0530

    added number-triangle

---
+++ 49. Number Triangle/number-triangle.c
@@ -0,0 +1,20 @@
+// Print the given pattern 
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+
+#include <stdio.h>
+int main(){
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        
+            printf("\n");
+    }
+}
