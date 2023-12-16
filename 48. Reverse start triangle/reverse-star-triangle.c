057fe26eff4247d80d5a35bc81486ebbb5c93453
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-16 14:30:14 +0530

    added reverse star pattern

---
+++ 48. Reverse start triangle/reverse-star-triangle.c
@@ -0,0 +1,21 @@
+// Print the given pattern
+
+// ****
+// ***
+// **
+// *
+
+#include <stdio.h>
+int main(){
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 4; j >= i; j--)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+        
+    }
+    
+}
