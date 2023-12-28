e9dca4128caed5b0953318689affbcd15e5b5db2
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-29 00:58:55 +0530

    added number square pyramid maze

---
+++ 72. DANGER = Number-square-Pyramid-maze/num-square-pyramid.c
@@ -0,0 +1,46 @@
+// Print the given Pattern
+
+// 4 4 4 4 4 4 4
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
+
+
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of lines: ");
+    scanf("%d", &n);
+    int min = 0;
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        for (int j = 1; j <= 2*n-1; j++)
+        {
+            int a = 1;
+            if (i>n)
+            {
+                a = 2*n-i;
+            }
+            int b = j;
+            if (b > n)
+            {
+                b = 2*n-j;
+            }
+            if (a<b)
+            {
+                min = a;
+            } else {
+                min = b;
+            }
+            printf("%d", n+1-min);
+            
+        }
+        printf("\n");
+        
+    }
+    return 0;
+    
+}
