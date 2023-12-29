27081510c5d332121cc09e62ca06af580e0f24ac
rishabh22204901 <rishabh22204901@gmail.com>
2023-12-29 14:46:21 +0530

    fix some issue in square pyramid

--- 72. DANGER = Number-square-Pyramid-maze/num-square-pyramid.c
+++ 72. DANGER = Number-square-Pyramid-maze/num-square-pyramid.c
@@ -19,7 +19,7 @@ int main(){
     {
         for (int j = 1; j <= 2*n-1; j++)
         {
-            int a = 1;
+            int a = i;
             if (i>n)
             {
                 a = 2*n-i;
