f5b6d2a035d793ac29e5ac54b7e068e9febdbdb4
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-03 13:19:34 +0530

    added print 1 to n by using recursion

---
+++ 85. Print 1 to n by using recursion/1-to-n-by-using-recursion.c
@@ -0,0 +1,21 @@
+// Print 1 to n 
+
+
+#include <stdio.h>
+
+int decreasing(int n){
+    if (n==1) return 1;
+    printf("%d\n", n);
+    return decreasing(n-1);
+}
+
+
+
+int main(){
+    int n;
+    printf("Print the number : ");
+    scanf("%d", &n);
+    int decrease = decreasing(n);
+    printf("%d", decrease);
+    return 0;
+}
