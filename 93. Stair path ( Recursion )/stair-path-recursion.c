cd3eb18a3b9b92f534060471a87b70c7684ed8c0
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-07 23:49:08 +0530

    added stair path

---
+++ 93. Stair path ( Recursion )/stair-path-recursion.c
@@ -0,0 +1,22 @@
+// No. of stairs
+// nth stair
+// can take single step or double step 
+// no. of ways so that the person reduces nth stairs
+
+
+#include <stdio.h>
+int number(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int totalWays = number(n-1) + number(n-2);
+        return totalWays;
+    
+    
+}
+int main(){
+    int n;
+    printf("Print the number : ");
+    scanf("%d", &n);
+    int stair = number(n);
+    printf("%d", stair);
+}
