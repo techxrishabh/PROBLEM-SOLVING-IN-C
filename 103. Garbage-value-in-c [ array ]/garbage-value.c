5dafa240659af94b58c68037be0985c2f7afd66d
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-15 12:51:39 +0530

    added Garbage-value [ array ]

---
+++ 103. Garbage-value-in-c [ array ]/garbage-value.c
@@ -0,0 +1,9 @@
+// GARBAGE VALUE IN C PROGRAMMING 
+
+#include <stdio.h>
+int main(){
+    int arr[4];
+    printf("%d", arr[0]);
+    return 0;
+}
+// it will different value every time we run it becuz we didinot give any value to this defined array
