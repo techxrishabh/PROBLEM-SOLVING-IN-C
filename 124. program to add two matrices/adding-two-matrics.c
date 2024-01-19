709e137c9228933c2d4e48c9beff29406108aafb
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-19 11:24:45 +0530

    added program to add two matrices without using ay third variable.

--- 124. program to add two matrices/adding-two-matrics.c
+++ 124. program to add two matrices/adding-two-matrics.c
@@ -1,8 +1,8 @@
-// Write a program to add two matrices.
+// Write a program to add two matrices without using ay third variable.
 
 #include <stdio.h>
 int main(){
-    int crr[2][2];
+
     int arr[2][2] = {{1, 2}, {3, 4}};
     int brr[2][2] = {{8, 9}, {5, 7}};
 
@@ -10,8 +10,7 @@ int main(){
     {
         for (int j = 0; j < 2; j++)
         {
-            crr[i][j] = arr[i][j] + brr[i][j];
-            printf("%d ", crr[i][j]);
+            printf("%d ", arr[i][j] + brr[i][j]);
         }
         printf("\n");
     }
