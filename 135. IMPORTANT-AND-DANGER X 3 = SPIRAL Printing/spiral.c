cddbed7e874dc436bb290e67f12c4b36dbf20a91
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-21 22:40:21 +0530

    added spiral printing

---
+++ 135. IMPORTANT-AND-DANGER X 3 = SPIRAL Printing/spiral.c
@@ -0,0 +1,57 @@
+// SPIRAL PRINTING
+
+// 1   2   3   4   5
+
+// 16  17  18  19  6
+
+// 15  24  25  20  7
+
+// 14  23  22  21  8
+
+// 13  12  11  10  9
+
+#include <stdio.h>
+int main(){
+    int n, count = 1;
+    printf("Print the value of N : \n");
+    scanf("%d", &n);
+    int arr[n][n];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n-i; j++)
+        {
+            arr[i][j] = count++;
+        }
+        for (int j = i + 1; j <= n - i - 1; j++)
+        {
+            arr[j][n-i-1] = count++;
+        }
+        for (int j = n-i-2; j >= i; j--)
+        {
+            arr[n-i-1][j] = count++;
+        }
+        for (int j = n - 2 - i; j > i; j--)
+        {
+            arr[j][i] = count++;
+        }
+        
+    }
+    for ( int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i][j] < 10)
+            {
+                printf("%d   ", arr[i][j]);
+            } else {
+                printf("%d  ", arr[i][j]);
+            }
+            
+        }
+        
+    printf("\n");
+    }
+
+}
