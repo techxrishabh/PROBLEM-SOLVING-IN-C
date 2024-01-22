18f8387c46e8139fa82e9097da5cfe19159fa89c
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-22 19:47:28 +0530

    add : predict the output

---
+++ 141. Predict the output ( strings )/predict.c
@@ -0,0 +1,14 @@
+// char str[] = "Physics Wallah";
+
+// Predict the output ?
+
+// (a) printf("%c", str[5]); 
+// (b) printf("%d", str[9]);
+
+#include <stdio.h>
+int main(){
+    char str[] = "Physics Wallah";
+
+    printf("%c", str[5]);  // c
+    printf("%d", str[9]);   // 97
+}
