e8f374456a110246ae3f400637bcbbe22ac0d2a9
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-23 23:06:43 +0530

    added structure type ‘book’ with name, price and number of pages as its attributes

---
+++ 149. Create a structure type ‘book’ with name, price and number of pages as its attributes/book-structure.c
@@ -0,0 +1,11 @@
+// Create a structure type ‘book’ with name, price and number of pages as its attributes
+
+#include <stdio.h>
+int main(){
+    struct book{
+        char name;
+        float price;
+        int numberOfPages;
+    };
+
+}
