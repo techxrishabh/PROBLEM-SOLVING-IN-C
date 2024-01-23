4e6bcb4c9e9e02b915f6ec933a6045f2c4808f22
rishabh22204901 <rishabh22204901@gmail.com>
2024-01-23 23:51:17 +0530

    added objects and classes

---
+++ 150. objects-and-classes/object-and-classes.c
@@ -0,0 +1,18 @@
+// Objects and Classes
+
+#include <stdio.h>
+int main(){
+    struct pokemon{
+    int hp;
+    int attack;
+    int speed;
+    };
+
+    struct pokemon pikachu;
+    pikachu.attack = 60;
+    pikachu.hp = 50;
+    pikachu.speed = 100;
+
+};
+
+// here pokemon is a class and pikachu is a object of class pokemon =======
