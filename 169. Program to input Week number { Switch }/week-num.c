83747d963f9d0d5aa0824c72378f70747d0afd9d
techxrishabh <rishabh22204901@gmail.com>
2024-02-01 19:35:16 +0530

    added program to enter week no.

---
+++ 169. Program to input Week number { Switch }/week-num.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the day number (1-7) : ");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid day\n");
+            break;
+    }
+}
